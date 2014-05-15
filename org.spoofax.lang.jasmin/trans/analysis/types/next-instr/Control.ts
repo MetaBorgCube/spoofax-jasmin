95cba4bf1c83a4ebe90db4f7ff38a68c095a3def
rmhartog <r.m.hartog@hotmail.com>
2014-05-15 14:11:02 +0200

    Added initial next-instr property for control-flow instructions.

---
+++ org.spoofax.lang.jasmin/trans/analysis/types/next-instr/Control.ts
@@ -0,0 +1,32 @@
+module types/next-instr/Control
+
+imports
+  include/JasminXT
+  analysis/names/extra-constructors
+
+type rules
+  IFEQ(t)      +
+  IFLT(t)      +
+  IFLE(t)      +
+  IFNE(t)      +
+  IFGT(t)      +
+  IFGE(t)      +
+  IFNULL(t)    +
+  IFNONNULL(t) +
+  IF_ICMPEQ(t) +
+  IF_ICMPLT(t) +
+  IF_ICMPLE(t) +
+  IF_ICMPNE(t) +
+  IF_ICMPGT(t) +
+  IF_ICMPGE(t) +
+  IF_ACMPEQ(t) +
+  IF_ACMPNE(t) has next-instr [NEXT(), t]
+  
+//  Table  +
+//  Lookup has next-instr []
+// TODO: all labels in the list
+  
+  GOTO(t)   +
+  GOTO_W(t) +
+  JSR(t)   +
+  JSR_W(t) has next-instr [t]
