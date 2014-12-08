1a3ba8bf59a7b4b6147cb93a5c657335c53fae9e
Gabriel Konat <gabrielkonat@gmail.com>
2014-12-08 13:47:09 +0100

    Make directories before writing class files.

--- org.spoofax.lang.jasmin/editor/java/org/spoofax/lang/jasmin/strategies/write_class_file_0_1.java
+++ org.spoofax.lang.jasmin/editor/java/org/spoofax/lang/jasmin/strategies/write_class_file_0_1.java
@@ -1,8 +1,8 @@
 package org.spoofax.lang.jasmin.strategies;
 
-import static org.spoofax.interpreter.core.Tools.asJavaString;
-import static org.spoofax.interpreter.core.Tools.isTermString;
+import static org.spoofax.interpreter.core.Tools.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -11,36 +11,36 @@ import org.strategoxt.lang.Context;
 import org.strategoxt.lang.Strategy;
 
 public class write_class_file_0_1 extends Strategy {
-	
-	public static write_class_file_0_1 instance = new write_class_file_0_1();
 
-	@Override
-	public IStrategoTerm invoke(Context context, IStrategoTerm current, IStrategoTerm fname) {
-		String path = null;
-		if (isTermString(fname)) {
-			path = asJavaString(fname);
-		} else {
-			return null;
-		}
-		
-		ClassGenerator cg = new ClassGenerator();
-		byte[] bt;
-		bt = cg.generateClass(context, current);
-		
-		if (bt == null) {
-			return null;
-		} else {
-			try {
-				context.getIOAgent().printError(path);
-				OutputStream fileOutputStream = context.getIOAgent().openFileOutputStream(path);
-				fileOutputStream.write(bt);
-				fileOutputStream.close();
-				return current;
-			} catch (IOException e) {
-				context.getIOAgent().printError(e.getMessage());
-				e.printStackTrace();
-			}
-			return null;
-		}
-	}
+    public static write_class_file_0_1 instance = new write_class_file_0_1();
+
+    @Override public IStrategoTerm invoke(Context context, IStrategoTerm current, IStrategoTerm fname) {
+        String path = null;
+        if(isTermString(fname)) {
+            path = asJavaString(fname);
+        } else {
+            return null;
+        }
+
+        ClassGenerator cg = new ClassGenerator();
+        byte[] bt;
+        bt = cg.generateClass(context, current);
+
+        if(bt == null) {
+            return null;
+        } else {
+            try {
+                context.getIOAgent().printError(path);
+                new File(path).mkdirs();
+                OutputStream fileOutputStream = context.getIOAgent().openFileOutputStream(path);
+                fileOutputStream.write(bt);
+                fileOutputStream.close();
+                return current;
+            } catch(IOException e) {
+                context.getIOAgent().printError(e.getMessage());
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
 }
