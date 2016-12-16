e7d8ac26b0101bb014eb80e94d9f4beb082528f8
Martijn Dwars <ikben@martijndwars.nl>
2016-12-16 10:30:24 +0100

    For grading, run Java in external process

--- jasmin/src/main/strategies/jasmin/strategies/execute_java_0_1.java
+++ jasmin/src/main/strategies/jasmin/strategies/execute_java_0_1.java
@@ -1,12 +1,10 @@
 package jasmin.strategies;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.PrintStream;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.IOUtils;
 import org.spoofax.interpreter.terms.IStrategoString;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.interpreter.terms.ITermFactory;
@@ -18,38 +16,39 @@ public class execute_java_0_1 extends Strategy {
 
 
     @Override public IStrategoTerm invoke(Context context, IStrategoTerm current, IStrategoTerm path) {
-        // remember system streams
-        PrintStream origOut = System.out;
-        PrintStream origErr = System.err;
-
-        // create temporary streams to collect output
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        ByteArrayOutputStream err = new ByteArrayOutputStream();
-
-        System.setOut(new PrintStream(out));
-        System.setErr(new PrintStream(err));
+        ITermFactory factory = context.getFactory();
 
         try {
-            // load class
-            File file = new File(((IStrategoString) path).stringValue());
-            try(URLClassLoader loader = new URLClassLoader(new URL[] { file.toURI().toURL() })) {
-                Class<?> cls = loader.loadClass(((IStrategoString) current).stringValue());
+            Process process = new ProcessBuilder("java", "-cp", ((IStrategoString) path).stringValue(), ((IStrategoString) current).stringValue()).start();
+            process.waitFor(10, TimeUnit.SECONDS);
 
-                // invoke main method
-                Method method = cls.getDeclaredMethod("main", new Class[] { String[].class });
-                method.invoke(null, (Object) new String[0]);
-            }
-        } catch(Exception e) {
+            // For some reason, without this call waitFor sometimes hangs?
+            process.exitValue();
+
+            String out = IOUtils.toString(process.getInputStream(), StandardCharsets.UTF_8);
+            String err = IOUtils.toString(process.getErrorStream(), StandardCharsets.UTF_8);
+
+            return factory.makeAppl(
+                factory.makeConstructor("Output", 2),
+                factory.makeString(out),
+                factory.makeString(err)
+            );
+        } catch (IOException e) {
             e.printStackTrace();
+
+            return factory.makeAppl(
+                factory.makeConstructor("Output", 2),
+                factory.makeString("An IOException occurred while executing java: " + e.getMessage()),
+                factory.makeString("An IOException occurred while executing java: " + e.getMessage())
+            );
+        } catch (InterruptedException e) {
+             e.printStackTrace();
+
+            return factory.makeAppl(
+                factory.makeConstructor("Output", 2),
+                factory.makeString("An InterruptedException occurred while executing java: " + e.getMessage()),
+                factory.makeString("An InterruptedException occurred while executing java: " + e.getMessage())
+            );
         }
-
-        // reset system streams
-        System.setErr(origErr);
-        System.setOut(origOut);
-
-        // return output collected in both temporary streams
-        ITermFactory factory = context.getFactory();
-        return factory.makeAppl(factory.makeConstructor("Output", 2), factory.makeString(out.toString()),
-            factory.makeString(err.toString()));
     }
 }
